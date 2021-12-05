c8d05a5812e8fe581368b3fdb0a8a48e379c1515
Kas S <82743976+Kas-S@users.noreply.github.com>
2021-12-05 16:59:34 +0500

    Delete datadoc directory

--- datadoc/base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-    path('', views.index, name="home"),
-    path('graphs/', views.graphs, name="graphs"),
-    # path('auth/signin/', views.signin, name='auth'),
-]
