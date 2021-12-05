c4434f599d7370723e4d0431be36c15770464bfb
Kas S <82743976+Kas-S@users.noreply.github.com>
2021-12-05 17:01:20 +0500

    Add files via upload

---
+++ datadoc/accounts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+# from django.contrib.auth.models import User
+
+# Create your models here.
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+#     first_name = models.CharField(max_length=200)
+#     last_name = models.CharField(max_length=200)
+#     username = models.CharField(max_length=36, unique=True)
+#     email = models.EmailField(max_length=200)
+#     date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+#     def __str__(self):
+#         return self.username
+
